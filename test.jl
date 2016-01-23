7695bb9b5e46e5a9fad4e4a6465c6c70e78ff410
EvgeneOskin <eoskin@crystalnix.com>
2016-01-23 14:25:27 +0600

    Fix coefficients for tests (fix floating issues).

--- test.jl
+++ test.jl
@@ -3,21 +3,22 @@ using ValidatedNumerics;
 using Base.Test;
 
 bVector = [
-    @interval(1, 2.1); @interval(0, 1); @interval(2, 3);
+    @interval(-2, 2); @interval(-2, 2);
 ];
 aMatrix = [
-    @interval(1, 2) @interval(0, 0) @interval(1, 2);
-    @interval(0, 0) @interval(1, 2) @interval(0, 0);
-    @interval(0, 0) @interval(0, 0) @interval(1, 2);
+    @interval(2, 4) @interval(-2, 1);
+    @interval(-1, 2) @interval(2, 4);
 ];
+preciseX = [
+    @interval(-0.333333); @interval(-0.333333);
+]
 m = [
-    1 0 1;
-    -0 -5.1 1;
-    -1 -2 10;
+    1 -2;
+    -0 -5.1;
 ];
 
-# @test m*bVector == ILESolver.reverseSTI(ILESolver.constituentMatrix(m)*ILESolver.STI(bVector))
+@test m*bVector == ILESolver.reverseSTI(ILESolver.constituentMatrix(m)*ILESolver.STI(bVector))
 
-# @test bVector == ILESolver.reverseSTI(ILESolver.STI(bVector))
+@test bVector == ILESolver.reverseSTI(ILESolver.STI(bVector))
 
 print(ILESolver.F.solve(aMatrix, bVector, 0.1, 1), "\n")
