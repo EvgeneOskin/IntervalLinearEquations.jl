2dc2e4c27506062f26f341e8d71c3a448fd355ba
EvgeneOskin <eoskin@crystalnix.com>
2016-06-26 00:10:27 +0600

    Rename ILESolver to IntervalLinearEquations.

--- test/test_solver.jl
+++ test/test_solver.jl
@@ -2,7 +2,7 @@ module SolverTests
 using Base.Test;
 using FactCheck;
 using ValidatedNumerics;
-using ILESolver;
+using IntervalLinearEquations;
 
 function interval_approx_eq(a_interval :: Interval{Float64}, b_interval :: Interval{Float64})
     isapprox(a_interval.lo, b_interval.lo) &&
@@ -19,7 +19,7 @@ facts("Solve 2x2 system with number matrix") do
         @interval(-1, 2) @interval(2, 4);
     ] :: Array{Interval{Float64}};
 
-    result = ILESolver.solve("G", eye(aMatrix), bVector, 0.1, 10, 1.0)
+    result = IntervalLinearEquations.solve("G", eye(aMatrix), bVector, 0.1, 10, 1.0)
     @fact result --> bVector
 end
 
@@ -33,7 +33,7 @@ facts("Solve 2x2 system with random matrix") do
     ] :: Array{Interval{Float64}};
     bVector = aMatrix * xVector
 
-    solution = ILESolver.solve("G", aMatrix, bVector, 0.1, 10, 1.0)
+    solution = IntervalLinearEquations.solve("G", aMatrix, bVector, 0.1, 10, 1.0)
     bFromSolution = aMatrix * solution
     @fact all(map(interval_approx_eq, bVector, bFromSolution)) --> true
     @fact all(map(interval_approx_eq, solution, xVector)) --> true
@@ -52,7 +52,7 @@ facts("Solve 2x2 system with interval matrix") do
         @interval(-0.33333333333333337, 0.33333333333333337);
     ] :: Array{Interval{Float64}};
 
-    solution = ILESolver.solve("G", aMatrix, bVector, 0.1, 10, 1.0)
+    solution = IntervalLinearEquations.solve("G", aMatrix, bVector, 0.1, 10, 1.0)
     bFromSolution = aMatrix * solution
     @fact all(map(interval_approx_eq, bVector, bFromSolution)) --> true
     @fact all(map(interval_approx_eq, solution, preciseX)) --> true
